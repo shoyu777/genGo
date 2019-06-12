c76b1d44049439eacf8e867dd8004c169923a4e8
shoyu777 <shoyu7@gmail.com>
2019-06-13 00:51:10 +0900

    implement binary search

--- genGo.go
+++ genGo.go
@@ -1,28 +1,71 @@
 package genGo
 
 import (
-	"fmt"
+	"encoding/csv"
+	"log"
+	"os"
+	"strconv"
 )
 
 type Gengo struct {
-	Kanji    string
-	Hiragana string
-	Romaji   string
+	StartYear int
+	Kanji     string
+	Romaji    string
+	Hiragana  string
 }
 
-var gengoList map[int]Gengo
+var gengoList []Gengo
 
 func init() {
-	g := Gengo{"a", "b", "c"}
-	g1 := Gengo{"令和", "れいわ", "Reiwa"}
-	fmt.Println(g)
-	gengoList = make(map[int]Gengo)
-	gengoList[1989] = g
-	gengoList[2019] = g1
+	gengoList = []Gengo{}
+
+	fr, err := os.Open("./src/mypkg/genGo/era_names.csv")
+	failOnError(err)
+
+	defer fr.Close()
+
+	r := csv.NewReader(fr)
+	r.Comma = ','
+
+	rows, err := r.ReadAll()
+	failOnError(err)
+
+	//set Gengo struct to a list from csv file
+	for _, v := range rows {
+		i, _ := strconv.Atoi(v[0])
+		g := Gengo{i, v[1], v[2], v[3]}
+		gengoList = append(gengoList, g)
+	}
+
 }
 
 //Search to seach gengo from YYYY
-func Search(i int) Gengo {
-	fmt.Println("search")
-	return gengoList[2019]
+func Search(target int) Gengo {
+	head := 0
+	tail := len(gengoList) - 1
+	center := 0
+
+	if gengoList[tail].StartYear <= target {
+		return gengoList[tail]
+	} else {
+		for head <= tail {
+			center = (head + tail) / 2
+
+			if gengoList[center].StartYear <= target && target < gengoList[center+1].StartYear {
+				return gengoList[center]
+			} else if gengoList[center].StartYear < target {
+				head = center + 1
+			} else {
+				tail = center - 1
+			}
+		}
+	}
+
+	return gengoList[1]
+}
+
+func failOnError(err error) {
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
 }
